e7059472f60f5ddff7dce5d5451b5d6ef86719c7
leonardoedgar <leonardo_edgar98@outlook.com>
2023-08-08 12:16:36 +0800

    initial build

---
+++ precache-manifest.108beff64b0b5983146396d598c44415.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf667e2ac3071c2ceab645189924070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165d597ed779c7f42198",
+    "url": "/static/css/main.bb71b34c.chunk.css"
+  },
+  {
+    "revision": "3c7c67726ea4549c7980",
+    "url": "/static/js/2.d0c4397e.chunk.js"
+  },
+  {
+    "revision": "165d597ed779c7f42198",
+    "url": "/static/js/main.35bc2289.chunk.js"
+  },
+  {
+    "revision": "b449bc4e6a1ee18b7675",
+    "url": "/static/js/runtime-main.d0908954.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "d040cc26ef54249a2882f39bc110fea4",
+    "url": "/static/media/chessboard_pattern.d040cc26.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "f2ab926d87364e927799a5f6648f835e",
+    "url": "/static/media/deep_learning_ai_logo.f2ab926d.png"
+  },
+  {
+    "revision": "3ad54ab3a0b4c76b26a1093a74ebda7b",
+    "url": "/static/media/defect_detection.3ad54ab3.png"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "f7fd51923edbd96d0f7ad859a031884c",
+    "url": "/static/media/eureka_robotics_logo.f7fd5192.png"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0a85f7ddfe7fd04db2982f81b8e20815",
+    "url": "/static/media/garuda_robotics_logo.0a85f7dd.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "f03071f2482840b3273890a386dc0c5b",
+    "url": "/static/media/handeye.f03071f2.gif"
+  },
+  {
+    "revision": "f22017d45f8613f689191f15345bd6b7",
+    "url": "/static/media/ieee_ras_logo.f22017d4.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "83435e745156e70010208af863410c64",
+    "url": "/static/media/mobile_manip.83435e74.gif"
+  },
+  {
+    "revision": "8dae03bbacd9e8207b45d367e6e1ca53",
+    "url": "/static/media/monitor_infra_one.8dae03bb.png"
+  },
+  {
+    "revision": "018f1cb0322afd6cd59b8bd1c3216e2f",
+    "url": "/static/media/monitor_infra_two.018f1cb0.png"
+  },
+  {
+    "revision": "8a110d5a949b4121a387b349c3f0c895",
+    "url": "/static/media/myself.8a110d5a.png"
+  },
+  {
+    "revision": "eb40cb98d99972d048440ee38d0aa773",
+    "url": "/static/media/ntu_logo.eb40cb98.png"
+  },
+  {
+    "revision": "7eba9cd52a7d9aa9a8dfdc55313ddb8a",
+    "url": "/static/media/ntu_logo_bigger.7eba9cd5.png"
+  },
+  {
+    "revision": "68f8a2e94fd31b1a0e6e304a8471d04b",
+    "url": "/static/media/pick_and_place.68f8a2e9.gif"
+  },
+  {
+    "revision": "a2f8cdd9be23eccd4ab31265ce94391a",
+    "url": "/static/media/pierian_data_logo.a2f8cdd9.png"
+  },
+  {
+    "revision": "605004415cd708555ada80ac76df615d",
+    "url": "/static/media/pierian_training_logo.60500441.jpg"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "1cc29ee3abbe0c3073a79d69f40e94ba",
+    "url": "/static/media/stereovision.1cc29ee3.gif"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "8ef48989a757c3eb8ca4de4e9d2f32e6",
+    "url": "/static/media/tfordesign_logo.8ef48989.png"
+  },
+  {
+    "revision": "0ffe7a91aa765c02082e797ba3f5ced3",
+    "url": "/static/media/the_learn_programming_logo.0ffe7a91.png"
+  },
+  {
+    "revision": "9863b1b56d69bafd5ccbe9a26ff2f477",
+    "url": "/static/media/thrusters_forward.9863b1b5.gif"
+  },
+  {
+    "revision": "89f90427005a3696c67dab96e60ba7a9",
+    "url": "/static/media/thrusters_left.89f90427.gif"
+  },
+  {
+    "revision": "549423147ff56c9d4bba87fbcf9bf030",
+    "url": "/static/media/thrusters_rotate.54942314.gif"
+  },
+  {
+    "revision": "45178b98204643280bbeb1dd28ff6d61",
+    "url": "/static/media/tilting_angle.45178b98.png"
+  },
+  {
+    "revision": "912b7809cc97c0dfebce31ee99908af0",
+    "url": "/static/media/wacu-wacu.912b7809.gif"
+  }
+]);
